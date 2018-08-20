384965d5a2fd0979f14728045bac49d663b00b33
Vikas Pal <pal.vikas210@gmail.com>
2018-08-20 23:46:55 +0530

    Added comment

--- app/src/main/java/com/example/cas/volumemap/MainActivity.java
+++ app/src/main/java/com/example/cas/volumemap/MainActivity.java
@@ -7,8 +7,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-    public static int counter=0;
-    TextView tv;
+
+    private static int counter = 0;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +20,22 @@ public class MainActivity extends Activity {
         tv.setText("Counter:"+String.valueOf(counter));
     }
 
+
+    //override onKeyDown() method to remap volume keys
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if(keyCode==KeyEvent.KEYCODE_VOLUME_DOWN){
             tv.setText("Counter : " + String.valueOf(--counter));
-            Toast.makeText(this,"Volume Down Downed",Toast.LENGTH_SHORT).show();
-        return true;
+            Toast.makeText(this, "Pressed Volume Down", Toast.LENGTH_SHORT).show();
+            return true;
         }
         if(keyCode==KeyEvent.KEYCODE_VOLUME_UP){
             tv.setText("Counter : " + String.valueOf(++counter));
-            Toast.makeText(this,"Volume Up Uped",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Pressed Volume Up", Toast.LENGTH_SHORT).show();
             return true;
-        }
-        else{
+        } else{
             return super.onKeyDown(keyCode,event);
         }
-
 
 
     }
